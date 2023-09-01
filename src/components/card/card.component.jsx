5a3e25f6915cbe604cdbc7a50ca15537346c1e9c
Sasguez <marioard5644@hotmail.com>
2023-09-01 07:57:05 -0500

    icon button and subtitle

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@
 import { Link } from "react-router-dom";
 import Button from "../button/button.component";
 import "./card.styles.scss";
-const Card = ({ quantity, link, linktitle, total, title, icon, status, priority, imagen, homeCard, color,button, buttonText, subtitle }) => {
+const Card = ({ quantity, link, total, title, icon, status, priority, imagen, homeCard, color,button,linkButton, buttonText, buttonIcon, subtitle }) => {
   console.log(subtitle)
   return (
     <div className={homeCard ? "card-div-container-x4" : "card-div-container"}>
@@ -18,43 +18,43 @@ const Card = ({ quantity, link, linktitle, total, title, icon, status, priority,
         {status && <span className={`server-status ${status}`}></span>}
       </div>
       {subtitle && (
-        <div className="subtitle">
-          <h4>{subtitle}</h4>
-        </div>
+        <p style={{fontFamily: "Sora", fontWeight:700, fontSize: "14px"}}>{subtitle}</p>
       )}
       <p style={color ? {color:color} : {}}>{quantity}</p>
       {imagen ? (
         <img src={imagen} alt="" />
       ):""}
       {button && (
-        <Link to='/Assets-Discovery'>
+        <Link to={linkButton}>
           <Button 
             type={"succes"}
             size="medium"
             position={"left"}
-            style={{marginRight:10, marginBotom:"5px"}}
+            style={{marginRight:"5px", marginBotom:"5px"}}
             // onClick={handleClose}
             // style={confirmButton}
-            // icons={<img style={{marginRight:"5px"}} src={checkIcon} alt="check" />}
+            icons={buttonIcon}
           >
             {buttonText}
           </Button>
         </Link>
       )}
-      <div className="card-links">
-        {total ? (<p>
-          Total :<span>{total}</span>
-        </p>) : ""
-        }
-        {
-          link ? 
-        (<Link to={link}>{linktitle?linktitle:"Overview"}</Link>
-        ) : ""
-        }
-        {
-          // todo <Link to={link}>Overview</Link>
-        }
-      </div>
+      {!button && 
+        <div className="card-links">
+          {total ? (<p>
+            Total :<span>{total}</span>
+          </p>) : ""
+          }
+          {
+            link ? 
+          (<Link to={link}>Overview</Link>
+          ) : ""
+          }
+          {
+            // todo <Link to={link}>Overview</Link>
+          }
+        </div>
+      }
     </div>
   );
 };
--- src/components/routes/inventoryList/inventoryList.component.jsx
+++ src/components/routes/inventoryList/inventoryList.component.jsx
@@ -18,6 +18,10 @@ import TablePartnerAccounts from "./TablePartnerAccounts/TablePartnerAccounts"
 import TableUserAccounts from "./TableUserAccounts/TableUserAccounts";
 import { Grid } from '@mui/material';
 
+let codeIcon = <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M20.99 7.80725V7.81V16.19C20.99 17.8111 20.5106 18.9851 19.7429 19.7529C18.9751 20.5206 17.8011 21 16.18 21H7.81C6.18889 21 5.01517 20.5207 4.24755 19.7521C3.47978 18.9834 3 17.8068 3 16.18V7.81C3 6.18887 3.47936 5.01486 4.24711 4.24711C5.01486 3.47936 6.18887 3 7.81 3H16.19C17.8113 3 18.9849 3.47944 19.7512 4.24666C20.5173 5.01368 20.9945 6.18676 20.99 7.80725ZM10.7705 10.173C9.9948 9.0103 8.92411 8.06488 7.67117 7.43755C6.8086 6.99847 5.75088 7.34525 5.32316 8.21763C4.89538 9.07964 5.22922 10.1381 6.11994 10.568C6.77556 10.8973 7.3518 11.3947 7.78865 12.0012C7.36102 12.6055 6.78441 13.1036 6.11691 13.4335L6.11691 13.4335L6.11279 13.4356C5.24924 13.8673 4.88114 14.9155 5.32801 15.7921C5.63626 16.4039 6.25123 16.75 6.89 16.75C7.16591 16.75 7.43822 16.6789 7.66721 16.5644C8.92179 15.9371 9.99388 14.991 10.7704 13.8272C11.5165 12.7202 11.5165 11.28 10.7705 10.173ZM13 16.75H17C17.9623 16.75 18.75 15.9623 18.75 15C18.75 14.0377 17.9623 13.25 17 13.25H13C12.0377 13.25 11.25 14.0377 11.25 15C11.25 15.9623 12.0377 16.75 13 16.75Z" fill="#0FB5AE" stroke="#FFFFFF" strokeWidth="2"/>
+</svg>
+
 const cards = [
   {
     title: "Last scan performed",
@@ -26,6 +30,9 @@ const cards = [
     status: null,
     button: true,
     buttonText: "Repeat scan",
+    buttonIcon: codeIcon,
+    linkButton: '/Assets-Discovery',
+    link: "/",
   },
   {
     title: "New devices",
@@ -89,13 +96,15 @@ const InventoryList =  () => {
               title={card.title}
               priority={card.priority}
               quantity={card.quantity}
+              subtitle={card?.subtitle ? card.subtitle : null}
               icon={card.icon}
               total={card.total}
               status={card.status}
               link={card.link}
               button={card.button}
               buttonText={card.buttonText}
-              subtitle={card?.subtitle ? card.subtitle : null}
+              buttonIcon={card.buttonIcon}
+              linkButton={card.linkButton}
             />
           </Grid>
         ))}
